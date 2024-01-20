9add10bed50175fe8ccd90c3dfc06aacef0c8f30
pantaelaman <calvin@theophoffs.com>
2024-01-20 13:58:31 -0500

    Started apriltag positioning

--- apriltag_test.py
+++ apriltag_test.py
@@ -1,17 +1,11 @@
+import cv2
 from dt_apriltags import Detector
 import numpy as np
-import argparse
-import pathlib
-import yaml
-import math
-import cv2
 import os
+import yaml
+import re
 
 def main():
-    parser = argparse.ArgumentParser(prog='Apriltag Detector')
-    parser.add_argument("intrinsics_file", type=pathlib.Path)
-    args = parser.parse_args()
-
     at_detector = Detector(searchpath=['apriltags'],
                            families='tag36h11',
                            nthreads=2,
@@ -20,38 +14,32 @@ def main():
 
     camera_params = [0] * 4
 
-    with open(args.intrinsics_file) as f:
+    with open("intrinsics.yaml") as f:
         params = yaml.safe_load(f)
         camera_params[0] = params["fx"]
         camera_params[1] = params["fy"]
         camera_params[2] = params["cx"]
         camera_params[3] = params["cy"]
 
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            continue
-
-        cv2.imshow("frame", frame)
-
-        grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    directory = 'out'
+    for filename in os.listdir(directory):
+        fname = os.path.join(directory, filename)
+        print(f"--- {fname} ---")
+        img = cv2.imread(fname)
+        grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         all_tags = at_detector.detect(grayscale,
                                       estimate_tag_pose=True,
                                       camera_params=camera_params,
                                       tag_size=0.163525)
-
         for tag in all_tags:
-            dx = tag.pose_t[0]
-            dz = tag.pose_t[2]
-            theta = math.atan(dx/dz)
-            rmat = np.array(((np.cos(theta), np.sin(theta))))
-            print(f"{round(math.degrees(theta), 2)}")
-            break
-
-        if cv2.waitKey(1) == ord('q') & 0xff:
-            break
+            pose_t = tag.pose_t
+            estimated = np.linalg.norm([pose_t[0], pose_t[2]]) * 3.28084
+            actual = int(re.findall(r"\d+", fname)[0])
+            print(tag.tag_id)
+            print(f"Actual: {actual}\nEstimated: {estimated}")
+            print(pose_t)
+            print("")
+        print("\n")
 
 if __name__ == "__main__":
     main()
