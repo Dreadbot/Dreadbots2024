1035193d1051bc865622070132161c936f0a36fa
FyNnNiL <fynn.soren.nielsen@gmail.com>
2024-01-30 16:39:54 -0500

    distance detection

--- note_detection.py
+++ note_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def main():
     cap = cv2.VideoCapture(4)
@@ -7,6 +8,8 @@ def main():
     while True:
         ret, frame = cap.read()
         if not ret: break 
+
+        bndreclist = []
 
         hsvim = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -22,13 +25,52 @@ def main():
 
         imrange = cv2.bitwise_and(not_imrange, ring_imrange)
 
-        kernel = np.ones((5, 5), np.uint8)
+        kernel = np.ones((3, 3), np.uint8)
 
         blurim = cv2.GaussianBlur(imrange, (1, 1), 0)
         erodeim = cv2.erode(blurim, kernel)
         dilateim = cv2.dilate(erodeim, kernel)
+
+        contours, hierarchy = cv2.findContours(dilateim, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        flen = 652
+        cam_pitch = 25
+        cam_height = 21.5
         
-        cv2.imshow("frame", dilateim)
+        for c in contours:
+            if cv2.contourArea(c) < 1200:
+                continue
+            
+            x, y, w, h = cv2.boundingRect(c)
+
+            a = x + (w//2)
+            z = y + (h//2)
+            
+            bndreclist.append([x,y,w,h,a,z])
+            
+        for b in bndreclist:
+            cv2.rectangle(frame, (b[0], b[1]), (b[0]+b[2], b[1]+b[3]), (0, 255, 0), 2)
+
+            pixel_y = b[5] - 240
+
+            center_coordinates = b[4], b[5]
+
+            color = (255, 0, 0)
+
+            cv2.circle(frame, center_coordinates, 5, color, 2)
+
+            c = math.atan(pixel_y / flen)
+            d = math.radians(90 - cam_pitch)
+            e = flen / math.cos(c)
+
+            trix = math.sin(d-c) * e
+            triy = math.cos(d-c) * e
+
+            distance_to_note = trix * (cam_height / triy)
+
+            print(distance_to_note)
+        
+        cv2.imshow("frame", frame)
                  
         if cv2.waitKey(1) == ord('q') & 0xff:
             break
