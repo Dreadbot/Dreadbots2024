8d235928363032c1755c74e962fb001cbd56f829
Arman Buyukbozkirli <arman12cb@gmail.com>
2024-03-20 20:27:55 -0400

    Parabolic arm lock on

--- src/main/java/frc/robot/commmands/armCommands/ArmTargetCommand.java
+++ src/main/java/frc/robot/commmands/armCommands/ArmTargetCommand.java
@@ -14,38 +14,64 @@ import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Shooter;
 import util.misc.WaypointHelper;
 
+
+
 public class ArmTargetCommand extends Command {
     private final Arm arm;
     private final Drive drive;
-    private double distanceToSpeakerBase;
+    private double fixedAngle = 1.41372;
+    private double armToEndOfPizza = .2413; // I don't know about this number you guys probably have to measure
+    private double g = -9.8;
+    private double deltaX = 0; // delta x and delta h are random values that are close enough to their actual
+    private double hBase = .2; // should be height of arm pivet, I don't know this value
+    private double deltaH = .8;
+    private double targetH = 2.1336;
+    private double armLength = .6096;
+    private double speakerHoodOffset = 0.3048;
     private double originToBase = .2032;
     private Translation2d speakerPos = WaypointHelper.getSpeakerPos();
     private DriverStation.Alliance alliance = WaypointHelper.getAlliance();
-    private double speakerHoodOffset = 0.3048;
-    private double speakerHeight = 2.1336;
-    private double angleBoxArm = 1.41372;
-    private double armLength = .6096;
+    private double hNought;
+    private double D;
+    private double a;
+    private double b;
+    private double c; 
+    private double horizontalAngle;
     private double armAngle;
-    
+    private double armRot;
     private Translation2d speakerHood;
+
+    private double vNought = 16; // I don't know what you guys want this to be
+
     public ArmTargetCommand(Arm arm, Drive drive) {
         this.arm = arm;
         this.drive = drive;
         addRequirements(arm);
-        if (alliance == DriverStation.Alliance.Red) {
+        if(alliance == DriverStation.Alliance.Red) {
             speakerHoodOffset = -0.3048;
         }
         speakerHood = new Translation2d(speakerPos.getX() + speakerHoodOffset, speakerPos.getY());
     }
 
     @Override
-    public void execute() { 
+    public void execute(){
         Pose2d pos = drive.getPoseEstimator().getEstimatedPosition();
-        double groundDistToSpeaker = Math.hypot(speakerHood.getX() - pos.getX(), speakerHood.getY() - pos.getY() - 0.2032);
-        double pivotToHoodTheta = Math.atan2(speakerHeight, groundDistToSpeaker);
-        double distPivotToHood = Math.hypot(groundDistToSpeaker, speakerHeight);
-        
-        armAngle = Math.asin((armLength * Math.sin(angleBoxArm)) / distPivotToHood) + angleBoxArm - pivotToHoodTheta;
-        arm.setReference(new State(armAngle, 0));
+        double targetBoxX = Math.hypot(speakerHood.getX() - pos.getX(), speakerHood.getY() - pos.getY() - originToBase);
+
+        for(int i = 0; i < 20; i++) {
+            hNought = hBase + deltaH;
+            D = targetBoxX + deltaX;
+            a = -(g * (Math.pow(D, 2))) / (2 * (Math.pow(vNought, 2)));
+            b = D;
+            c = hNought + a - targetH;
+
+            horizontalAngle = Math.atan((-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2 * a));
+            armAngle = fixedAngle - horizontalAngle;
+            deltaH = armLength * Math.sin(fixedAngle - horizontalAngle) + armToEndOfPizza * Math.sin(horizontalAngle);
+            deltaX = armLength * Math.cos(armAngle);
+        }
+        armRot = armAngle / (2 * Math.PI);
+        SmartDashboard.putNumber("Vision Target Command Angle", armRot);
+        arm.setReference(new State(armRot, 0));
     }
 }
